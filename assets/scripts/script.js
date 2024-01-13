75dd64a6c14b3641b9e716ed20db3e5c5ed4f99b
rkvrpl <rkvrpl@icloud.com>
2024-01-13 21:54:00 +0300

    added in script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,39 @@
 "use strict"
-// Используя JavaScript, добавьте обработчик события отправки формы (`submit`), который будет выполнять следующие действия:
 
-// - Отменять действие по умолчанию для события `submit`
-// - Отображать сообщение об ошибке рядом с каждым полем при обнаружении ошибки валидации
-// - Кнопка отправки должна быть неактивна (`disabled`), пока все поля формы не будут правильно заполнены и не будет отмечен чекбокс согласия с условиями
-// - Если форма проходит проверку валидности, выводите в консоль значения полей формы и очищайте форму
+const form = document.forms.form,
+    name = form.elements.name,
+    email = form.elements.email,
+    age = form.elements.age,
+    profession = form.elements.profession,
+    checkbox = form.elements.checkbox,
+    button = form.elements.button;
 
-// 4 Бонусное задание: реализуйте дополнительные обработчики событий `focus` и `blur` для каждого поля
-const form = document.forms.form;
-const name = form.elements.name;
-const email = form.elements.email;
-const age = form.elements.age;
-const woman = form.elements.woman;
-const man = form.elements.man;
-const profession = form.elements.profession;
-const pass = form.elements.pass;
-const checkbox = form.elements.checkbox;
-const button = form.elements.button;
-
-form.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-
-    // form.reset(); //Сброс полей формы
+    function createError(input , text){
+        const parent = input.parentNode;
+        const errorLabel = document.createElement('p');
+        errorLabel.textContent = text; 
+        parent.append(errorLabel);
+    }
+    function validation(elem){
+        if(elem.value === ''){
+            createError(elem, 'Поле не заполнено или заполнено неверно');
+        }
+    }
+    form.addEventListener('submit', function (evt) {
+        evt.preventDefault();
+        validation(name);
+        validation(email);
+        validation(age);
+        validation(profession);
+        checkbox.addEventListener('click', () =>{
+            if(checkbox.checked === true){
+                button.disabled = false;
+            }
+            if(checkbox.checked === false){
+                button.disabled = true;
+            }
+        });
+        
+        form.reset();
     });
+    
